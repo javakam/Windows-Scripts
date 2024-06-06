33a581febf8ce68b02ac64abe8ff7da70563645c
javakam <jooybao@foxmail.com>
2024-06-06 10:00:17 +0800

    start up

--- 一键启动.bat
+++ 一键启动.bat
@@ -10,9 +10,28 @@ echo.Martin's Blog  https://javakam.github.io
 color 1f
 echo.-----------------------------------------------------------
 
+echo Starting QQ...
+start "" "C:\Program Files\Tencent\QQNT\QQ.exe"
+::start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
+::当使用代理软件时 ping 127.0.0.1 会报错
+::ping -n 1 -w 100 192.168.1.1 >nul
+
+echo Starting WeChat...
+start "" "C:\Program Files\Tencent\WeChat\WeChat.exe"
+
+echo Starting 浏览器...
+start /max C:\Users\MaChangBao\AppData\Local\360ChromeX\Chrome\Application\360ChromeX.exe
+
+echo Starting 向日葵...
+start "" "C:\Program Files\Oray\SunLogin\SunloginClient\SunloginClient.exe"
+
+echo Starting 哔哩哔哩...
+start "" "C:\Program Files\bilibili\哔哩哔哩.exe"
+
+echo.-----------------------------------------------------------
+
 ::echo Starting idea64...
 ::start "" "C:\fastwork\JetBrains\IntelliJ IDEA 2023.1.5\bin\idea64.exe"
-::timeout /t 0.1 /nobreak >nul
 
 ::echo Starting Android Studio...
 ::start "" C:\fastwork\android-studio\bin\studio64.exe
@@ -20,30 +39,8 @@ echo.-----------------------------------------------------------
 ::echo Starting 微信开发者工具...
 ::start "" C:\fastwork\微信web开发者工具\微信开发者工具.exe
 
-echo Starting QQ...
-start "" "C:\Program Files\Tencent\QQNT\QQ.exe"
-::start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
-timeout /t 0.1 /nobreak >nul
-
-echo Starting WeChat...
-start "" "C:\Program Files\Tencent\WeChat\WeChat.exe"
-timeout /t 0.1 /nobreak >nul
-
-echo Starting 浏览器...
-start "" C:\Users\MaChangBao\AppData\Local\360ChromeX\Chrome\Application\360ChromeX.exe
-timeout /t 0.1 /nobreak >nul
-
-echo Starting 向日葵...
-start "" "C:\Program Files\Oray\SunLogin\SunloginClient\SunloginClient.exe"
-timeout /t 0.1 /nobreak >nul
-
-echo Starting 哔哩哔哩...
-start "" "C:\Program Files\bilibili\哔哩哔哩.exe"
-timeout /t 0.1 /nobreak >nul
-
 ::echo Starting PhpStudy...
 ::start "" "C:\fastwork\phpstudy_pro\COM\phpstudy_pro.exe"
-::timeout /t 0.1 /nobreak >nul
 
 :end
 echo 执行完成
